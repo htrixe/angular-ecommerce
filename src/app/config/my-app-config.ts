df0d39ed712e6444b5ca3e7ea78cacefa5694982
humendes <hugo-manuel.mendes@altran.com>
2023-07-16 12:49:33 +0100

    Local storage  created

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oaaeavjdkbZVJnZt5d7',
+        issuer: 'https://dev-08122190.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
